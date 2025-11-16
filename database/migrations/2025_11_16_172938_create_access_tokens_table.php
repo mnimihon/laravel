4216cf93a9323073aa6860dee67f8f9aed938c4a
mhsivanov <mh.s.ivanov@yandex.ru>
2025-11-16 23:01:35 +0300

    Хранилище для Laravel-продукта // ДЗ

---
+++ database/migrations/2025_11_16_172938_create_access_tokens_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up() {
+        Schema::create('access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('token', 64);
+            $table->dateTime('last_used_at')->nullable();
+            $table->dateTime('expires_at')->nullable();
+            $table->dateTime('created_at')->useCurrent();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('access_tokens');
+    }
+};
